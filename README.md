d9f9d05d307fecf048bb7238334a44c27acb6f47
Keeghan, Damian (AU - Melbourne) <dkeeghan@deloitte.com.au>
2017-12-29 12:22:15 +1100

    First release as part of SlackViz.com

--- README.md
+++ README.md
@@ -1,2 +1,129 @@
-# slackformatter
+# slackformatter.js
 Formats messages from the Slack API into HTML
+
+## Prerequisites
+
+This library requires [js-emoji](https://github.com/iamcal/js-emoji).
+
+It can be installed from the git repo, or using `npm install emoji-js` in your project.
+
+## Installation
+
+This library uses the UMD wrapper and can be loaded as a browser global as `SLACKFORMATTER`. The following examples will all use the browser global, switch the terminology to match your import flavour of choice.
+
+## Usage
+
+There are three things you should do to setup slackformatter.js:
+
+1. Configure the options to your liking
+2. Add a list of users from the Slack instance so the formatter can parse user IDs into user names
+3. Add a list of custom emoji from the Slack instance
+
+### `SLACKFORMATTER.setOptions(options)`
+
+There are four options you can set for slackformatter.
+
+1. `emojiPath` - path to the default emoji images used by emoji-js. _Default `/assets/img/emoji/`_
+2. `channelClass` - class name for the channels. _Default `slack-channel`_
+3. `userClass` - class name for the users. _Default `slack-user`_
+4. `emojiClass` - class name for the emoji. _Default `slack-emoji`_
+
+You can pass through an object with one or more of these options to update the settings of the plugin. Be sure to do this before anything else.
+
+### `SLACKFORMATTER.addEmoji(emoji)`
+
+Do a Slack API call to `emoji.list` and you'll get an array of objects back from the response (`response[0].emoji`).
+
+Pass this array directly into `SLACKFORMATTER.addEmoji(emoji)`.
+
+```js
+doSlackAPICall('emoji.list').then(function(response) {
+    SLACKFORMATTER.addEmoji(response[0].emoji);
+});
+```
+
+_You may want to periodically update this is you have new custom emoji added all the time and you have a long running application._
+
+### `SLACKFORMATTER.addUsers(users)`
+
+Do a Slack API call to `users.list` and you'll get an array of objects back from the response (`response[0].members`).
+
+Pass this array directly into `SLACKFORMATTER.addUsers(users)`.
+
+```js
+doSlackAPICall('users.list').then(function(response) {
+    SLACKFORMATTER.addEmoji(response[0].members);
+});
+```
+
+_You may want to periodically update this is you have new users added all the time and you have a long running application._
+
+### `SLACKFORMATTER.get(text)`
+
+Once you have the custom emoji and users added, now all you need to do is call `SLACKFORMATTER.get(text)` where the text is any Slack formatted text (e.g. `message.text` or `message.file.initial_comment.comment`).
+
+## Output
+
+### General formatting
+
+#### Bold
+
+`*bold text*` becomes `<strong>bold text</strong>`.
+
+#### Italics
+
+`_italics text_` becomes `<em>italics text</em>`.
+
+
+#### Strikeout
+
+`~striked text~` becomes `<del>striked text</del>`.
+
+#### Code
+
+``code`` becomes `<code>code</code>`.
+
+#### Preformatted text
+
+````preformatted```` becomes `<pre>preformatted</pre>`.
+
+#### Links
+
+`<https://github.com/dkeeghan/slackformatter>` becomes `<a href="https://github.com/dkeeghan/slackformatter">https://github.com/dkeeghan/slackformatter</a>`
+
+#### Combinations
+
+slackformatter also supports combinations, so `*_bold italics_*` becomes `<strong><em>bold italics</em></strong>`.
+
+### Usernames
+
+`<@USER_ID>` becomes `<span class="slack-user">USERNAME</span>`. 
+
+
+Where `slack-user` is the class name you've specified in the options. By default it is `slack-user`.
+
+You can then use CSS pseudo elements to add an @ symbol.
+
+### Channels 
+
+`<#CHANNEL_ID|CHANNEL_NAME>` becomes `<span class="slack-channel">CHANNEL_NAME</span>`.
+
+Where `slack-channel` is the class name you've specified in the options. By default it is `slack-channel`.
+
+You can then use a CSS psuedo element to add a # symbol.
+
+### Default Emoji
+
+`:smile:` becomes `<span class="slack-emoji" style="background-image:url(/path/to/emoji/1f604.png)" data-codepoints="1f604"></span>`
+
+Where `/path/to/emoji` is where you've configured slackformatter to look for the images when you set the options. The default location is `/assets/img/emoji/`.
+
+### Custom Emoji
+
+`:party_parrot:` becomes `<span class="slack-emoji" style="background-image: url('https://emoji.slack-edge.com/ORG_ID/party_parrot/IMAGE_ID.gif')"></span>`
+
+Where `slack-emoji` is the class name you've specified in the options. By default it is `slack-emoji`.
+
+## Example
+
+You can see an example of slackformatter running on [SlackViz](https://slackviz.com).
---
+++ slackformatter.js
@@ -0,0 +1,283 @@
+/*! https://github.com/dkeeghan/slackformatter | BSD 3-Clause (http://opensource.org/licenses/BSD-3-Clause) */
+(function (root, factory) {
+    // UMD wrapper - Works with node, AMD and browser globals.
+    // Using the returnExports pattern as a guide:
+    // https://github.com/umdjs/umd/blob/master/templates/returnExports.js
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.SLACKFORMATTER = factory();
+    }
+}(this, function () {
+
+    var _users = {},
+		_customEmoji = {},
+		_emoji = new EmojiConvertor(),
+		_getUser,
+		_getCustomEmoji,
+		_parseSlackTag,
+		_getHTML,
+		_validateFormatMatch,
+		_formats = {},
+		_options,
+		addUsers,
+		addEmoji,
+		get,
+		setOptions;
+
+	_options = {
+		emojiPath: '/assets/img/emoji/',
+		channelClass: 'slack-channel',
+		userClass: 'slack-user',
+		emojiClass: 'slack-emoji'
+	}
+
+	_getUser = function(id) {
+		if (typeof (_users[id]) === 'object') {
+			return _users[id];
+		}
+
+		return false;
+	};
+
+	_getCustomEmoji = function(id) {
+		if (typeof (_customEmoji[id]) === 'string') {
+			return _customEmoji[id];
+		}
+
+		return false;
+	};
+
+	_parseSlackTag = function(tagContents, skipIdentifier) {
+		if (skipIdentifier) {
+			tagContents = tagContents.substr(1, tagContents.length - 1);
+		}
+
+		return tagContents.split('|');
+	};
+
+	_getHTML = function(tag, content, attrs) {
+		if (typeof (attrs) !== 'object') {
+			attrs = {};
+		}
+
+		var tagAndAttrs = tag;
+
+		for (var attr in attrs) {
+			if (attrs.hasOwnProperty(attr)) {
+				tagAndAttrs = tagAndAttrs + ' ' + attr + '="' + attrs[attr] + '"';
+			}
+		}
+
+		return '<' + tagAndAttrs + '>' + content + '</' + tag + '>';
+	};
+
+	_validateFormatMatch = function(match, html) {
+		var checkWhitespace = function(val) {
+			return /^\s?$/.test(val);
+		};
+
+		var mIndex = match.index,
+			mInput = match.input,
+			isValidBefore = (mIndex === 0),
+			isValidAfter = (mIndex === mInput.length - match[0].length);
+
+		if (isValidBefore === false) {
+			isValidBefore = checkWhitespace(mInput.substr(mIndex - 1, 1));
+		}
+
+		if (isValidAfter === false) {
+			isValidAfter = checkWhitespace(mInput.substr(mIndex + match[0].length, 1));
+		}
+
+		//if (isValidBefore && isValidAfter) {
+			return html;
+		//}
+
+		return false;
+	}
+
+	_formats.slackTags = {
+		regex: /<(.*?)>/g,
+		formatter: function(match) {
+			var type = match[1].substring(0, 1),
+				value = match[1];
+
+			if (type === '@' || type === '!') {
+				// users
+				var userTag = _parseSlackTag(value, true);
+
+				if (userTag.length === 2) {
+					return _getHTML('span', userTag[1], { class: _options.userClass });
+				}
+
+				var user = _getUser(userTag[0]);
+				user = (user) ? user.name : userTag[0];
+
+				return _getHTML('span', user, { class: _options.userClass });
+			} else if (type === '#') {
+				// channels
+				var channelTag = _parseSlackTag(value, true),
+					channel = (channelTag.length === 1) ? channelTag[0] : channelTag[1];
+
+				return _getHTML('span', channel, { class: _options.channelClass });
+			} else {
+				// links
+				var linkTag = _parseSlackTag(value),
+					linkTagText = (linkTag.length === 1) ? linkTag[0] : linkTag[1];
+
+				return _getHTML('a', linkTagText, { href: linkTag });
+			}
+		}
+	};
+
+	_formats.emoji = {
+		regex: /:([a-zA-Z0-9-_+]+):/g,
+		formatter: function(match) {
+			var name = match[1],
+				customEmoji = _getCustomEmoji(name);
+
+			// TODO Support more than just custom emoji
+			if (customEmoji !== false) {
+				return _getHTML('span', '', {
+					class: _options.emojiClass,
+					style: 'background-image: url(\'' + customEmoji + '\')'
+				});
+			}
+
+			_emoji.img_sets.apple.path = _options.emojiPath;
+			_emoji.replace_mode = 'img';
+			_emoji.allow_native = false;
+			_emoji.supports_css = true;
+			_emoji.use_sheet = false;
+
+			var img = _emoji.replace_colons(':' + name + ':');
+			img = img.replace(/class="(.*?)"/, 'class="' + _options.emojiClass + '"');
+
+			return img;
+		}
+	};
+
+	_formats.bold = {
+		regex: /\*([^\*]*?)\*/g,
+		formatter: function(match) {
+			return _validateFormatMatch(match, _getHTML('strong', match[1]));
+		}
+	};
+
+	_formats.italics = {
+		regex: /_([^_]*?)_/g,
+		formatter: function(match) {
+			return _validateFormatMatch(match, _getHTML('em', match[1]));
+		}
+	};
+
+	_formats.strike = {
+		regex: /~([^~]*?)~/g,
+		formatter: function(match) {
+			return _validateFormatMatch(match, _getHTML('del', match[1]));
+		}
+	};
+
+	_formats.preformatted = {
+		regex: /```([^```]*?)```/g,
+		htmlTag: 'pre',
+		formatter: function(match) {
+			return _validateFormatMatch(match, _getHTML('pre', match[1]));
+		}
+	};
+
+	_formats.code = {
+		regex: /`([^`]*?)`/g,
+		htmlTag: 'code',
+		formatter: function(match) {
+			return _validateFormatMatch(match, _getHTML('code', match[1]));
+		}
+	};
+
+	_formats.quote = {
+		regex: /^(?:>|&gt;)\s*(.+)/g,
+		formatter: function(match) {
+			return _validateFormatMatch(match, _getHTML('q', match[1]));
+		}
+	};
+
+	_formats.newline = {
+		regex: /(?:\r\n|\r|\n)/g,
+		formatter: function() {
+			return '<br/>';
+		}
+	};
+
+	addUsers = function(users) {
+		if (typeof(users) === 'object') {
+			_users = users;
+			return true;
+		} else if(typeof(users) === 'array') {
+			for (var i = 0, len = users.length; i < len; i += 1) {
+				_users[user.id] = users[i];
+			}
+
+			return true;
+		}
+
+		return false;
+	};
+
+	addEmoji = function(emoji) {
+		if (typeof(emoji) === 'object') {
+			_customEmoji = emoji;
+			return true;
+		}
+
+		return false;
+	};
+
+	get = function(text) {
+		if (typeof (text) !== 'string') {
+			throw new Error('Text supplied to SLACKFORMATTER.get() must be a string.');
+		}
+
+		for (var key in _formats) {
+			if (_formats.hasOwnProperty(key)) {
+				var format = _formats[key],
+					originalText = text,
+					matched;
+
+				// replace each of the instances of this format (could have 0 or many instances)
+				while ((matched = format.regex.exec(originalText)) !== null) {
+					var replaceWith = format.formatter(matched);
+
+					if (replaceWith) {
+						text = text.replace(matched[0], replaceWith);
+					}
+				}
+			}
+		}
+
+		return text;
+	}
+
+	setOptions = function(options) {
+		for (var key in options) {
+			if (_options.hasOwnProperty(key)) {
+				_options[key] = options[key];
+			}
+		}
+	};
+
+	return {
+		addUsers: addUsers,
+		addEmoji: addEmoji,
+		get: get,
+		setOptions: setOptions
+	};
+}));
